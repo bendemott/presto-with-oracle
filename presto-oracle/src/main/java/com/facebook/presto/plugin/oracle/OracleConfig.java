d7d5e74d2b717d6d9ef291fe2173722e3ca69039
Ben DeMott <Ben.DeMott@arrow.com>
2019-11-12 15:14:16 -0700

    implement starburst-like configuration options

--- presto-oracle/src/main/java/com/facebook/presto/plugin/oracle/OracleConfig.java
+++ presto-oracle/src/main/java/com/facebook/presto/plugin/oracle/OracleConfig.java
@@ -13,18 +13,87 @@
  */
 package com.facebook.presto.plugin.oracle;
 
+import com.facebook.presto.plugin.oracle.UnsupportedTypeHandling;
 import io.airlift.configuration.Config;
 import io.airlift.units.Duration;
-
+import java.math.RoundingMode;
 import javax.validation.constraints.Min;
-
 import java.util.concurrent.TimeUnit;
 
 public class OracleConfig
 {
+    public static int UNDEFINED_SCALE = -127;
+    private UnsupportedTypeHandling typeStrategy = UnsupportedTypeHandling.IGNORE;
     private boolean autoReconnect = true;
     private int maxReconnects = 3;
     private Duration connectionTimeout = new Duration(10, TimeUnit.SECONDS);
+    private boolean synonymsEnabled = false;
+    private int numberScale = UNDEFINED_SCALE;
+    private boolean undefinedScaleAsInteger = false;
+    private RoundingMode roundingMode = RoundingMode.UNNECESSARY;
+
+    // oracle.synonyms.enabled
+    // unsupported-type.handling-strategy {FAIL | IGNORE | CONVERT_TO_VARCHAR}
+
+    public boolean isSynonymsEnabled()
+    {
+        return synonymsEnabled;
+    }
+
+    @Config("oracle.synonyms.enabled")
+    public OracleConfig setSynonymsEnabled(boolean synonymsEnabled)
+    {
+        this.synonymsEnabled = synonymsEnabled;
+        return this;
+    }
+
+    public UnsupportedTypeHandling getUnsupportedTypeStrategy()
+    {
+        return typeStrategy;
+    }
+
+    @Config("unsupported-type.handling-strategy")
+    public OracleConfig setUnsupportedTypeStrategy(String typeStrategy)
+    {
+        this.typeStrategy = UnsupportedTypeHandling.valueOf(typeStrategy);
+        return this;
+    }
+
+    public RoundingMode getRoundingMode()
+    {
+        return roundingMode;
+    }
+
+    @Config("oracle.number.rounding-mode")
+    public OracleConfig setRoundingMode(String roundingMode)
+    {
+        this.roundingMode = RoundingMode.valueOf(roundingMode);
+        return this;
+    }
+
+    public boolean isUndefinedScaleInteger()
+    {
+        return undefinedScaleAsInteger;
+    }
+
+    @Config("oracle.number.zero-scale-as-integer")
+    public OracleConfig setUndefinedScaleInteger(boolean undefinedScaleAsInteger)
+    {
+        this.undefinedScaleAsInteger = undefinedScaleAsInteger;
+        return this;
+    }
+
+    public int getDefaultNumberScale()
+    {
+        return numberScale;
+    }
+
+    @Config("oracle.number.default-scale")
+    public OracleConfig setDefaultNumberScale(int numberScale)
+    {
+        this.numberScale = numberScale;
+        return this;
+    }
 
     public boolean isAutoReconnect()
     {
