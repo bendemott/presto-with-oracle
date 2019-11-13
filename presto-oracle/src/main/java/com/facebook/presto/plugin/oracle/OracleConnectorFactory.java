d0df9218f427ed772718856b36d982fcf9431fa6
Ben DeMott <Ben.DeMott@arrow.com>
2019-11-13 10:14:43 -0700

    implement driver check, and driver checksum

--- presto-oracle/src/main/java/com/facebook/presto/plugin/oracle/OracleConnectorFactory.java
+++ presto-oracle/src/main/java/com/facebook/presto/plugin/oracle/OracleConnectorFactory.java
@@ -13,23 +13,27 @@
  */
 package com.facebook.presto.plugin.oracle;
 
-import java.io.File;
-import java.net.URL;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+import java.io.InputStream;
+import java.io.FileInputStream;
+import java.security.MessageDigest;
 import com.facebook.presto.plugin.jdbc.JdbcHandleResolver;
 import com.facebook.presto.spi.ConnectorHandleResolver;
 import com.facebook.presto.spi.classloader.ThreadContextClassLoader;
 import com.facebook.presto.spi.connector.Connector;
 import com.facebook.presto.spi.connector.ConnectorContext;
 import com.facebook.presto.spi.connector.ConnectorFactory;
-//import com.facebook.presto.server.PluginManagerConfig;
-import com.google.common.base.Throwables;
+import com.facebook.presto.spi.function.FunctionMetadataManager;
+import com.facebook.presto.spi.function.StandardFunctionResolution;
+import com.facebook.presto.spi.relation.RowExpressionService;
+import com.google.common.hash.HashCode;
 import com.google.inject.Injector;
 import com.google.inject.Module;
 import io.airlift.bootstrap.Bootstrap;
 import io.airlift.log.Logger;
-
-import java.util.Map;
-
+import static com.google.common.base.Throwables.throwIfUnchecked;
 import static com.google.common.base.Preconditions.checkArgument;
 import static com.google.common.base.Strings.isNullOrEmpty;
 import static java.util.Objects.requireNonNull;
@@ -39,14 +43,15 @@ public class OracleConnectorFactory
 {
     private static final Logger LOG = Logger.get(OracleConnectorFactory.class);
     private final String name;
-    private final String ORACLE_IMPORT_TEST = "oracle.jdbc.driver.OracleDriver";
+    private final String ORACLE_IMPORT_TEST = "oracle.jdbc.OracleDriver";
+    // ORACLE_IMPORT_PROP - use to override ORACLE_IMPORT_TEST value
     private final String ORACLE_IMPORT_PROP = "com.facebook.presto.plugin.oracle.ImportTest";
     private final Module module;
     private final ClassLoader classLoader;
 
+
     public OracleConnectorFactory(String name, Module module, ClassLoader classLoader)
     {
-
         checkArgument(!isNullOrEmpty(name), "name is null or empty");
         this.name = name;
         this.module = requireNonNull(module, "module is null");
@@ -65,17 +70,26 @@ public class OracleConnectorFactory
     @Override
     public ConnectorHandleResolver getHandleResolver()
     {
-        return new JdbcHandleResolver();
+        return new JdbcHandleResolver(); // why override this?
     }
 
     @Override
-    public Connector create(String connectorId, Map<String, String> requiredConfig, ConnectorContext context)
+    public Connector create(String catalogName, Map<String, String> requiredConfig, ConnectorContext context)
     {
         requireNonNull(requiredConfig, "requiredConfig is null");
         requireOracleDriver();
 
+        // based off of JdbcConnectorFactory
         try (ThreadContextClassLoader ignored = new ThreadContextClassLoader(classLoader)) {
-            Bootstrap app = new Bootstrap(new OracleModule(connectorId), module);
+
+            Bootstrap app = new Bootstrap(
+                    binder -> {
+                        binder.bind(FunctionMetadataManager.class).toInstance(context.getFunctionMetadataManager());
+                        binder.bind(StandardFunctionResolution.class).toInstance(context.getStandardFunctionResolution());
+                        binder.bind(RowExpressionService.class).toInstance(context.getRowExpressionService());
+                    },
+                    new OracleModule(catalogName),
+                    module);
 
             Injector injector = app
                     .strictConfig()
@@ -86,50 +100,81 @@ public class OracleConnectorFactory
             return injector.getInstance(OracleConnector.class);
         }
         catch (Exception e) {
-            throw Throwables.propagate(e);
+            throwIfUnchecked(e);
+            throw new RuntimeException(e);
         }
     }
 
     /**
      * The Oracle Driver is provided for us via a JAR present in the classpath.
      * If it's not present raise a useful error.
+     * If it is present, inform the user which JAR they've provided via its SHA1 hash.
      */
     private void requireOracleDriver() {
-        String overrideImport = System.getProperty(ORACLE_IMPORT_TEST);
-        String oracleImport = ORACLE_IMPORT_PROP;
-
-        // setInstalledPluginsDir
-        // @Config("plugin.dir")
-        // com.facebook.airlift.configuration.Config;
-        // pluginManagerConfig expected = new PluginManagerConfig()
-        //
-        // .setInstalledPluginsDir(new File("plugins-dir"))
-
-        // https://github.com/prestodb/presto/blob/8d5d5e67e1e2276e9e2a1fc02f471e6d0a020c89/presto-raptor/src/test/java/com/facebook/presto/raptor/RaptorQueryRunner.java
-        //import com.facebook.presto.metadata.SessionPropertyManager;
-
-        //File plugin_path = new PluginManagerConfig().getInstalledPluginsDir();
-        //String pluginDir = config.getInstalledPluginsDir();
+        String overrideImport = System.getProperty(ORACLE_IMPORT_PROP);
+        String oracleImport = ORACLE_IMPORT_TEST;
 
         if (overrideImport != null && !overrideImport.isEmpty()) {
             oracleImport = overrideImport;
         }
+
+        Map<String,String> oracleDriverHashes = new HashMap<>();
+        oracleDriverHashes.put("a483a046eee2f404d864a6ff5b09dc0e1be3fe6c", "11.2.0.4 (11g Release 2)"); // JDK 6-8
+        oracleDriverHashes.put("5543067223760fc2277fe3f603d8c4630927679c", "11.2.0.4 (11g Release 2)"); // JDK 5
+        oracleDriverHashes.put("a2348e4944956fac05235f7cd5d30bf872afb157", "12.1.0.1 (12c Release 1)"); // JDK 7-8
+        oracleDriverHashes.put("5d5d3e7a6b217ddc0c1c4c6d14b352e8b04453ef", "12.1.0.1 (12c Release 1)"); // JDK 6
+        oracleDriverHashes.put("7c9b5984b2c1e32e7c8cf3331df77f31e89e24c2", "12.1.0.2 (12c Release 1)"); // JDK 7-8
+        oracleDriverHashes.put("76f2f84c383ef45832b3eea6b5fb3a6edb873b93", "12.1.0.2 (12c Release 1)"); // JDK 6
+        oracleDriverHashes.put("60f439fd01536508df32658d0a416c49ac6f07fb", "12.2.0.1 (12c Release 2)"); // JDK 8
+        oracleDriverHashes.put("4acaa9ab2b7470fa80f0a8ec416d7ea86608ac8c", "18.3 (18c)"); // JDK 8
+        oracleDriverHashes.put("bba59347e68c9416d14fcc9a9209e869f842e48d", "19.3 (19c)"); // JDK 10
+        oracleDriverHashes.put("967c0b1a2d5b1435324de34a9b8018d294f8f47b", "19.3 (19c)"); // JDK 8
+
+        String jarFile = "";
         try {
             final Class<?> generic;
-            generic = Class.forName(oracleImport, false, this.getClass().getClassLoader()); // forName(String name, boolean initialize, ClassLoader loader)
-            URL location = generic.getProtectionDomain().getCodeSource().getLocation();
-            LOG.info("loading Oracle jdbc driver from '%s'", location);
+            generic = Class.forName(oracleImport, false, this.getClass().getClassLoader());
+            jarFile = generic.getProtectionDomain().getCodeSource().getLocation().getPath();
+            LOG.info("Oracle JDBC driver loaded from '%s'", jarFile);
         } catch (ClassNotFoundException ex) {
-            String.format("Oracle JDBC driver not found, unable to load class %s.%n"
-                        + "Oracle Plugin JAR files can be downloaded from https://www.oracle.com/database/technologies/appdev/jdbc-downloads.html"
-                        + "JAR files should be placed in prestos plugin directory ");
-            RuntimeException ex2 = new RuntimeException(ex.getMessage());
+            String msg = String.format("Oracle JDBC driver not found, unable to load class '%s'%n"
+                        + "Oracle Plugin JAR files can be downloaded from https://www.oracle.com/database/technologies/appdev/jdbc-downloads.html%n"
+                        + "JAR files should be placed in the presto plugin directory", oracleImport);
+            RuntimeException ex2 = new RuntimeException(msg);
             ex2.initCause(ex);
-            throw ex2;
+            LOG.error(ex2);
+        }
+
+        try {
+            String jarSha1 = createChecksum(jarFile);
+            String jarVersion = oracleDriverHashes.getOrDefault(jarSha1, "unknown");
+            LOG.info("Oracle Driver: %s, sha1: %s", jarVersion, jarSha1);
+        } catch (Exception ex) {
+            LOG.info("Unable to check Oracle JAR Version: %s", ex.toString());
         }
     }
 
-    // SHA1 hashes
-    // 60f439fd01536508df32658d0a416c49ac6f07fb
-    //
+    private static String createChecksum(String filename) throws Exception {
+        InputStream fis =  new FileInputStream(filename);
+        try {
+            byte[] buffer = new byte[1024 * 10];
+            MessageDigest complete = MessageDigest.getInstance("SHA-1");
+            int numRead;
+
+            do {
+                numRead = fis.read(buffer);
+                if (numRead > 0) {
+                    complete.update(buffer, 0, numRead);
+                }
+            } while (numRead != -1);
+
+            // convert the sha1 hash to HEX
+            byte[] hashBytes = complete.digest();
+            String hashHex = HashCode.fromBytes(hashBytes).toString();
+            return hashHex;
+        } finally {
+            // close the input stream quietly
+            try {if (fis != null) fis.close();} catch(IOException ex) {}
+        }
+    }
 }
