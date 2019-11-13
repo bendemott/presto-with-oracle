cdf66ec44c0a30edf60a4d9dc8c2c7e23833e933
Ben DeMott <Ben.DeMott@arrow.com>
2019-11-13 10:11:12 -0700

    add support for oracle.number.default-scale, unsupported-type.handling-strategy, oracle.number.rounding-mode

--- presto-oracle/src/main/java/com/facebook/presto/plugin/oracle/OracleClient.java
+++ presto-oracle/src/main/java/com/facebook/presto/plugin/oracle/OracleClient.java
@@ -16,78 +16,143 @@ package com.facebook.presto.plugin.oracle;
 import com.facebook.presto.plugin.jdbc.*;
 import com.facebook.presto.spi.ConnectorSession;
 import com.facebook.presto.spi.PrestoException;
-import com.facebook.presto.spi.type.CharType;
-import com.facebook.presto.spi.type.Type;
-import com.facebook.presto.spi.type.VarcharType;
-import com.google.common.base.Throwables;
-import com.google.common.collect.ImmutableSet;
 import com.facebook.presto.spi.SchemaTableName;
-
+import com.facebook.presto.spi.StandardErrorCode;
+import com.facebook.presto.spi.type.Decimals;
+import com.google.common.collect.ImmutableSet;
+import io.airlift.log.Logger;
 import oracle.jdbc.OracleDriver;
 
 import javax.annotation.Nullable;
 import javax.inject.Inject;
-
+import java.math.RoundingMode;
 import java.sql.*;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+import java.util.Optional;
 
-import static com.google.common.collect.Iterables.getOnlyElement;
+import static com.facebook.presto.plugin.jdbc.JdbcErrorCode.JDBC_ERROR;
+import static com.facebook.presto.plugin.jdbc.StandardReadMappings.integerReadMapping;
 import static com.facebook.presto.plugin.jdbc.StandardReadMappings.jdbcTypeToPrestoType;
 import static com.facebook.presto.spi.StandardErrorCode.NOT_SUPPORTED;
-import static com.facebook.presto.spi.type.BigintType.BIGINT;
-import static com.facebook.presto.spi.type.BooleanType.BOOLEAN;
-import static com.facebook.presto.spi.type.CharType.createCharType;
-import static com.facebook.presto.spi.type.DateType.DATE;
 import static com.facebook.presto.spi.type.DecimalType.createDecimalType;
-import static com.facebook.presto.spi.type.DoubleType.DOUBLE;
-import static com.facebook.presto.spi.type.IntegerType.INTEGER;
-import static com.facebook.presto.spi.type.RealType.REAL;
-import static com.facebook.presto.spi.type.SmallintType.SMALLINT;
-import static com.facebook.presto.spi.type.TimeType.TIME;
-import static com.facebook.presto.spi.type.TimestampType.TIMESTAMP;
-import static com.facebook.presto.spi.type.TinyintType.TINYINT;
-import static com.facebook.presto.spi.type.VarbinaryType.VARBINARY;
 import static com.facebook.presto.spi.type.VarcharType.createUnboundedVarcharType;
 import static com.facebook.presto.spi.type.VarcharType.createVarcharType;
+import static com.google.common.collect.Iterables.getOnlyElement;
 import static java.lang.Math.max;
 import static java.lang.Math.min;
-import static com.facebook.presto.plugin.jdbc.JdbcErrorCode.JDBC_ERROR;
 
+/**
+ * OracleClient is where the actual connection to Oracle is built
+ * DriverConnectionFactory is what does the work of actually connecting and applying JdbcOptions
+ */
 public class OracleClient
         extends BaseJdbcClient
 {
-    private final String META_DB_NAME_FIELD = "TABLE_CATALOG";
+    private static final Logger LOG = Logger.get(OracleClient.class);
+    private static final String META_DB_NAME_FIELD = "TABLE_SCHEM";
+    private static final String QUERY_SCHEMA_SYNS = String.format("SELECT distinct(OWNER) AS %s FROM SYS.ALL_SYNONYMS", META_DB_NAME_FIELD);
+    private static final String QUERY_TABLE_SYNS = "SELECT TABLE_OWNER, TABLE_NAME FROM SYS.ALL_SYNONYMS WHERE OWNER = ? AND SYNONYM_NAME = ?;";
+    private OracleConfig oracleConfig;
 
     @Inject
-    public OracleClient(JdbcConnectorId connectorId, BaseJdbcConfig config)
+    public OracleClient(JdbcConnectorId connectorId, BaseJdbcConfig config, OracleConfig oracleConfig)
+            throws SQLException
     {
-        super(connectorId, config, "\"", new DriverConnectionFactory(new OracleDriver(), config));
+        super(connectorId, config, Character.toString('"'), new DriverConnectionFactory(new OracleDriver(), config));
+        this.oracleConfig = oracleConfig;
     }
 
     @Override
+    /**
+     * SELECT distinct(owner) AS DATABASE_SCHEM FROM SYS.ALL_SYNONYMS;
+     * SCHEMA synonyms must be included in the Schema List, ALL_SYNONYMS are any synonym visible by the current user.
+     */
     protected Collection<String> listSchemas(Connection connection)
     {
+        ImmutableSet.Builder<String> schemaNames = ImmutableSet.builder();
         try (ResultSet resultSet = connection.getMetaData().getSchemas()) {
-            ImmutableSet.Builder<String> schemaNames = ImmutableSet.builder();
             while (resultSet.next()) {
-                // Database Names in "TABLE_CATALOG" instead of "TABLE_SCHEM" for Oracle
+                // Schema Names are in "TABLE_SCHEM" for Oracle
                 String schemaName = resultSet.getString(META_DB_NAME_FIELD);
-                // skip internal schemas
-                if (!schemaName.equalsIgnoreCase("information_schema")) {
-                    schemaNames.add(schemaName);
+                if(schemaName == null) {
+                    LOG.error("connection.getMetaData().getSchemas() returned null schema name");
+                    continue;
                 }
+                // skip internal schemas
+                if (schemaName.equalsIgnoreCase("information_schema")) {
+                    continue;
+                }
+
+                schemaNames.add(schemaName);
             }
-            return schemaNames.build();
         }
         catch (SQLException e) {
             throw new PrestoException(JDBC_ERROR, e);
+        }
+
+        // Merge schema synonyms with all schema names.
+        try {
+            schemaNames.addAll(listSchemaSynonyms(connection));
+        } catch (PrestoException ex2) {
+            LOG.error(ex2);
+        }
+        return schemaNames.build();
+    }
+
+    private Collection<String> listSchemaSynonyms(Connection connection) {
+        ImmutableSet.Builder<String> schemaSynonyms = ImmutableSet.builder();
+        try {
+            Statement stmt = connection.createStatement();
+            ResultSet resultSet = stmt.executeQuery(QUERY_SCHEMA_SYNS);
+            while (resultSet.next()) {
+                String schemaSynonym = resultSet.getString(META_DB_NAME_FIELD);
+                schemaSynonyms.add(schemaSynonym);
+            }
+        } catch (SQLException e) {
+            throw new PrestoException(
+                    JDBC_ERROR, String.format("Failed retrieving schema synonyms, query was: %s", QUERY_SCHEMA_SYNS));
+        }
+
+        return schemaSynonyms.build();
+    }
+
+    /**
+     * Retrieves the underlying schema and table-name for a given SCHEMA.TABLE synonym alias.
+     * Returns null if no synonym is found.
+     *
+     * @param connection
+     * @param schemaTableName
+     * @return
+     */
+    private SchemaTableName getTableSynonym(Connection connection, SchemaTableName schemaTableName) {
+        try {
+            // Find a synonym that matches the current schema/table name.
+            PreparedStatement stmt = connection.prepareStatement(QUERY_TABLE_SYNS);
+            stmt.setString(1, schemaTableName.getSchemaName());
+            stmt.setString(2, schemaTableName.getTableName());
+            ResultSet rs = stmt.executeQuery();
+            if(rs.next()) {
+                String schema = rs.getString("TABLE_OWNER");
+                String table = rs.getString("TABLE_NAME");
+                SchemaTableName toSchemaTable = new SchemaTableName(schema, table);
+                return toSchemaTable;
+            } else {
+                return null;
+            }
+        } catch (SQLException e) {
+            PrestoException ex = new PrestoException(
+                    JDBC_ERROR, String.format("Failed retrieving table synonym, query was: %s", QUERY_TABLE_SYNS));
+            ex.initCause(e);
+            throw ex;
         }
     }
 
     @Override
     /**
      * Retrieve information about tables/views using the JDBC Drivers DatabaseMetaData api,
-     * Include "SYNONYMS" - functionality specific to Oracle
+     * Include "SYNONYM" - functionality specific to Oracle
      */
     protected ResultSet getTables(Connection connection, Optional<String> schemaName, Optional<String> tableName)
             throws SQLException
@@ -99,7 +164,7 @@ public class OracleClient
                 connection.getCatalog(),
                 escapeNamePattern(schemaName, escape).orElse(null),
                 escapeNamePattern(tableName, escape).orElse(null),
-                new String[] {"TABLE", "VIEW", "SYNONYM"});
+                new String[] {"TABLE", "VIEW", "SYNONYM", "GLOBAL TEMPORARY", "LOCAL TEMPORARY"});
     }
 
     @Nullable
@@ -107,18 +172,21 @@ public class OracleClient
     public JdbcTableHandle getTableHandle(JdbcIdentity identity, SchemaTableName schemaTableName)
     {
         try (Connection connection = connectionFactory.openConnection(identity)) {
+            // TODO needs to be replaced to include synonyms (if enabled)
             String remoteSchema = toRemoteSchemaName(identity, connection, schemaTableName.getSchemaName());
             String remoteTable = toRemoteTableName(identity, connection, remoteSchema, schemaTableName.getTableName());
+
+            List<JdbcTableHandle> tableHandles = new ArrayList<>();
             try (ResultSet resultSet = getTables(connection, Optional.of(remoteSchema), Optional.of(remoteTable))) {
-                List<JdbcTableHandle> tableHandles = new ArrayList<>();
                 while (resultSet.next()) {
                     tableHandles.add(new JdbcTableHandle(
                             connectorId,
-                            schemaTableName,
-                            resultSet.getString("TABLE_CAT"),
-                            resultSet.getString(META_DB_NAME_FIELD),
-                            resultSet.getString("TABLE_NAME")));
+                            schemaTableName,                                  // schemaTableName
+                            resultSet.getString("TABLE_CAT"),    // catalog
+                            resultSet.getString(META_DB_NAME_FIELD),         // schema
+                            resultSet.getString("TABLE_NAME"))); // table name
                 }
+
                 if (tableHandles.isEmpty()) {
                     return null;
                 }
@@ -139,16 +207,126 @@ public class OracleClient
         return resultSet.getString(META_DB_NAME_FIELD);
     }
 
+    /**
+     * Return an anonymous method that acts as a type mapper for the given column type.
+     * Each method is called a ReadMapping, which reads data in and converts the JDBC type to a supported Presto Type
+     * For more details see OracleReadMappings.java
+     *
+     * See: https://github.com/prestodb/presto/blob/3060c65a1812c6c8b0c2ab725b0184dbad67f0ed/presto-base-jdbc/src/main/java/com/facebook/presto/plugin/jdbc/StandardReadMappings.java
+     *
+     * JdbcRecordCursor is what calls this method
+     *
+     * @param session
+     * @param typeHandle
+     * @return
+     */
     @Override
     public Optional<ReadMapping> toPrestoType(ConnectorSession session, JdbcTypeHandle typeHandle)
-    //protected Type toPrestoType(int jdbcType, int columnSize, int decimalDigits)
     {
+        // The COLUMN_SIZE column specifies the column size for the given column. For numeric data, this is the maximum precision of the column
+
+        // -- Get configuration options from Oracle catalog ---------------------------------------
+        RoundingMode roundMode = oracleConfig.getRoundingMode();
+        int defaultScale = oracleConfig.getDefaultNumberScale();
+        UnsupportedTypeHandling unsupportedMode = oracleConfig.getUnsupportedTypeStrategy();
+        boolean undefinedNumberScaleAsInt = oracleConfig.isUndefinedScaleInteger();
+
+        // -- Get Column Field Size  --------------------------------------------------------------
         int columnSize = typeHandle.getColumnSize();
-        // TODO JSON support
+
+        // -- Handle JDBC to Presto Type Mappings -------------------------------------------------
+        Optional<ReadMapping> readType = Optional.empty();
         switch (typeHandle.getJdbcType()) {
-            // handle Oracle specific types
+            case Types.DATE:
+                // Oracle DATE values may store hours, minutes, and seconds, so they are mapped to TIMESTAMP in Presto.
+                break;
+            case Types.DECIMAL:
+            case Types.NUMERIC:
+                ReadMapping readFunc;
+                // decimalDigits provides NUMERIC Scale (to the right of the decimal place)
+                int decimalDigits = typeHandle.getDecimalDigits();
+                int absScale = Math.abs(decimalDigits);
+                // columnSize provides NUMERIC Precision (total significant digits)
+                int realPrecision = columnSize + max(-decimalDigits, 0);
+                int logicalPrecision = min(realPrecision, Decimals.MAX_PRECISION); // Map decimal(p, -s) (negative scale) to decimal(p+s, 0).
+                int logicalScale = min(max(decimalDigits, 0), Decimals.MAX_PRECISION);
+                String typeName = JDBCType.valueOf(typeHandle.getJdbcType()).getName();
+
+                boolean unsupported = true;
+                String reason = null;
+
+                if(decimalDigits == 0 && defaultScale == OracleConfig.UNDEFINED_SCALE && !undefinedNumberScaleAsInt) {
+                    reason = "type has no scale (0) and 'oracle.number.default-scale' is not set";
+                } else if(roundMode.equals(RoundingMode.UNNECESSARY)) {
+                    if (decimalDigits == 0) {
+                        reason = String.format("scale isn't set (0) - unknown scale");
+                    } else if (realPrecision > Decimals.MAX_PRECISION) {
+                        reason = String.format("precision exceeds Presto MAX_PRECISION of %d",
+                                Decimals.MAX_PRECISION);
+                    } else if (absScale > Decimals.MAX_PRECISION) {
+                        reason = String.format("scale exceeds Presto MAX_PRECISION of %d",
+                                Decimals.MAX_PRECISION);
+                    } else {
+                        unsupported = false;
+                    }
+                } else {
+                    unsupported = false;
+                }
+
+                if(unsupported) {
+                    LOG.info("!!!!!!!!!!!!!! in unsupported");
+                    // The data-type is unsupported in the current configuration
+                    reason = String.format("JDBC DataType Unsupported - %s(%d,%d) %s %n... You can change this behavior" +
+                                    "by modifying the configuration parameters 'oracle.number.rounding-mode' and " +
+                                    "'unsupported-type.handling-strategy'",
+                            typeName,
+                            decimalDigits,
+                            columnSize,
+                            reason);
+                    switch(unsupportedMode) {
+                        case IGNORE:
+                            LOG.warn(reason);
+                            break;
+                        case FAIL:
+                            throw new PrestoException(StandardErrorCode.GENERIC_INTERNAL_ERROR, reason);
+                        case CONVERT_TO_VARCHAR:
+                            readFunc = OracleReadMappings.stringDecimalReadMapping(createUnboundedVarcharType());
+                            readType = Optional.of(readFunc);
+                            break;
+                    }
+                } else {
+                    LOG.info("!!!!!!!!!!!! in supported");
+                    if(decimalDigits == 0 && undefinedNumberScaleAsInt == true) {
+                        LOG.info("!!!!!!!!!!!! in integerReadMapping");
+                        readFunc = integerReadMapping();
+                        readType = Optional.of(readFunc);
+                    } else {
+                        LOG.info("!!!!!!!!!!!! in supported roundDecimalReadMapping");
+                        // This is the 'normal' happy path, use our custom ReadMapping function to parse the decimal value
+                        // or round the value to the needed scale
+                        if(logicalScale == 0) {
+                            logicalScale = defaultScale;
+                        }
+                        readFunc = OracleReadMappings.roundDecimalReadMapping(
+                                createDecimalType(logicalPrecision, logicalScale), realPrecision, roundMode);
+                        readType = Optional.of(readFunc);
+                    }
+                }
+                break;
             default:
-                return jdbcTypeToPrestoType(typeHandle);
+                // use standard read mappings
+                readType = jdbcTypeToPrestoType(typeHandle);
         }
+
+        if(readType.isPresent()) { // The readType is empty
+            // data type was unhandled
+            int precision = typeHandle.getColumnSize();
+            int scale = typeHandle.getDecimalDigits();
+            int typeCode = typeHandle.getJdbcType();
+            String typeName = JDBCType.valueOf(typeCode).getName();
+            LOG.warn("Unhandled Column Type %s:%s [%s, %s] (precision, scale)", typeName, typeCode, precision, scale);
+        }
+
+        return readType;
     }
 }
